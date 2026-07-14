fed779a14ea50dd1b569aaa6703b95fc80de1260
VeedhaanXD <veedhaanxd07@gmail.com>
2026-07-15 01:56:48 +0530

    Cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["https://your-future-vercel-url.vercel.app"], // You will add the real URL here later
+  oorigin: ["http://localhost:5173", "https://vges.onrender.com/"],
   credentials: true
 }));
 app.use(express.json());
